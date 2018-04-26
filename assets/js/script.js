a66f2c973915c107cfd6dec83ad3a28f0adc77bd
thisisgene <emdo2000@gmail.com>
2018-04-26 17:43:06 +0200

    Initial commit.

---
+++ assets/js/script.js
@@ -0,0 +1,77 @@
+var oneSelected = false;
+var viewArray = ['none', 'space', 'city', 'mountain', 'people'];
+
+$(document).ready(function() {
+
+  $('.part').on('mouseover', function(e) {
+    var $this = $(e.target);
+    var id = $this.attr('id');
+    var $back = $('.home-container');
+
+    if (!oneSelected) {
+      switch (id) {
+        case '1':
+          $back.css('background-image', 'url("assets/img/space.jpg")');
+          $('#2').addClass('shine top-shine');
+          $('#3').addClass('shine left-shine');
+          break;
+        case '2':
+          $back.css('background-image', 'url("assets/img/city.jpg")');
+          $('#1').addClass('shine bottom-shine');
+          $('#4').addClass('shine left-shine');
+          break;
+        case '3':
+          $back.css('background-image', 'url("assets/img/mountain.jpg")');
+          $('#1').addClass('shine right-shine');
+          $('#4').addClass('shine top-shine');
+          break;
+        case '4':
+          $back.css('background-image', 'url("assets/img/people.jpg")');
+          $('#2').addClass('shine right-shine');
+          $('#3').addClass('shine bottom-shine');
+          break;
+      }
+      if (!$this.hasClass('hovered')) {
+        $this.addClass('hovered');
+        $('.part:not(.hovered)').addClass('not-hovered');
+      }
+    }
+
+    $this.on('click', function(e) {
+      oneSelected = true;
+      $.get('/ajax_views/'+viewArray[id]+'.html', function(res) {
+        console.log(viewArray[id]);
+        $this.addClass('is-selected');
+        $('.not-hovered').addClass('go-away').removeClass('not-hovered shine top-shine right-shine bottom-shine left-shine');
+        $('.full-container').html(res).addClass('is-active');
+      });
+
+
+    });
+  });
+  $('.part').on('mouseout', function(e) {
+    if (oneSelected) {
+      return;
+    }
+    var $this = $(e.target);
+    if ($this.hasClass('hovered')) {
+      $this.removeClass('hovered');
+      $('.part:not(.hovered)').removeClass('not-hovered shine top-shine right-shine bottom-shine left-shine');
+
+    }
+
+  });
+
+
+});
+
+function returnHome() {
+  console.log('back');
+  $('.is-selected').removeClass('is-selected');
+  $('.is-active').removeClass('is-active');
+  $('.not-hovered').removeClass('not-hovered');
+  $('.hovered').removeClass('hovered');
+  $('.go-away').removeClass('go-away');
+  oneSelected = false;
+
+};
